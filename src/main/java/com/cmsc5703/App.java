28c896f65a1071326272d7e7ea567b49c78db54a
Eddy Lau <eddylau0328@gmail.com>
2023-04-04 13:57:19 +0800

    feat: build jar file

--- src/main/java/com/cmsc5703/App.java
+++ src/main/java/com/cmsc5703/App.java
@@ -27,13 +27,15 @@ public class App
 
         // args[0] = name of the input directory
         // args[1] = output file (result of the sorted list)
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1])); 
 
-        Date date = new Date();
-        Timestamp timestamp = new Timestamp(date.getTime());
-        String outputPathString = "./output/" + timestamp.toString();
-
-        FileInputFormat.setInputPaths(conf, new Path("./input"));
-        FileOutputFormat.setOutputPath(conf, new Path(outputPathString));
+        // Testing
+        // Date date = new Date();
+        // Timestamp timestamp = new Timestamp(date.getTime());
+        // String outputPathString = "./output/" + timestamp.toString();
+        // FileInputFormat.setInputPaths(conf, new Path("./input"));
+        // FileOutputFormat.setOutputPath(conf, new Path(outputPathString));
 
         try {
             // Run job
